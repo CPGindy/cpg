f9d61baecf9ad686248fa60b7853897f1f424fd4
BishalGhimire <bisheshwor.ghimire@selu.edu>
2021-09-02 19:18:36 +0000

    update to product packaging

--- ssi_product_packaging/models/sale_order.py
+++ ssi_product_packaging/models/sale_order.py
@@ -1,14 +1,20 @@
-from odoo import models, fields, api
+import io
+import base64
+import logging
+from odoo.exceptions import UserError
 
+from odoo import models, fields, api, _
+
+_logger = logging.getLogger(__name__)
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
     
     product_packaging_id = fields.Many2one('product.packaging',string='Product Packaging')
-    
-    
+        
     
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
     
     product_packaging_id = fields.Many2one('product.packaging',string='Product Packaging')
+    
