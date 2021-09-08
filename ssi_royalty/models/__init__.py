c3bb4eb9d3298d71e3ba02add527dc20b43ed95c
BishalGhimire <bisheshwor.ghimire@selu.edu>
2021-09-08 17:55:03 +0000

    test build

--- ssi_royalty/models/__init__.py
+++ ssi_royalty/models/__init__.py
@@ -5,5 +5,5 @@ from . import license_license
 from . import license_item
 from . import license_product
 from . import product_product
-from . import account_move
+# from . import account_move
 from . import royalty_pool
