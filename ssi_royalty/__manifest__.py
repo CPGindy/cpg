8ca93e4e0a7ec3e65e65b668e53582ae6522fc01
BishalGhimire <bisheshwor.ghimire@selu.edu>
2021-09-13 13:25:09 -0500

    test new dev branch

--- ssi_royalty/__manifest__.py
+++ ssi_royalty/__manifest__.py
@@ -14,7 +14,7 @@
     'author': "Bishal Ghimire - Systems Services Inc",
     'website':  "http://www.ssibtr.com",
     'category': 'SSI',
-    'version': '0.1.0',
+    'version': '0.2.0',
 
     'depends': ['base', 'product','account', 'mail'],
 
