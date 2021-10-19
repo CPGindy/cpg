4e16e8302158cc201c66937e4d8ee899e06e62a1
BishalGhimire <bisheshwor.ghimire@selu.edu>
2021-10-19 15:59:15 -0500

    updated field defination

--- ssi_packing/models/res_partner.py
+++ ssi_packing/models/res_partner.py
@@ -4,4 +4,4 @@ from odoo import models, fields, api
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    store_number = fields.Char(string="Store Number", string="Partner/Store Number")
+    store_number = fields.Char(string="Store Number", help="Partner/Store Number")
