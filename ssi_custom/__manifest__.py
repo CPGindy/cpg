46c12d8d344d3621714c753c3faee8abbc5c8ffc
BishalGhimire <bisheshwor.ghimire@selu.edu>
2021-11-17 20:23:17 -0600

    Added new xls export for shipment(9622)

---
+++ ssi_custom/__manifest__.py
@@ -0,0 +1,27 @@
+# Systems Services, Inc.
+
+{
+    'name': "SSI Custom",
+
+    'summary': """
+        Module is for customizing base Odoo functionality for CPG""",
+
+    'description': """
+        Customizes base Odoo functionalities and add new one if applies
+    """,
+
+    'author': "Bishal Ghimire - Systems Services Inc",
+    'website':  "http://www.ssibtr.com",
+    'category': 'SSI',
+    'version': '0.1.0',
+    'depends': ['base', 'stock'],
+
+    'data': [
+        'report/ssi_stock_picking_report.xml',
+    ],
+    'demo': [
+    ],
+    'cloc_exclude': [
+        "./**/*"
+    ]
+}
