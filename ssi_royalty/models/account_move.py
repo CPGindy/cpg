754c3045a5543adc33dcf0027cd0ad4be71f8e68
BishalGhimire <bisheshwor.ghimire@selu.edu>
2021-12-17 13:57:08 -0600

    Update to add invoice product for all royalty(10637)

--- ssi_royalty/models/account_move.py
+++ ssi_royalty/models/account_move.py
@@ -68,6 +68,7 @@ class AccountMove(models.Model):
                                 data = {
                                         'licensed_item' : lic_prod.license_item_id.id,
                                         'license_product_id': lic_prod.id,
+                                        'invoice_product_id': lic_prod.product_id.id,
                                         'license_id' : lic_prod.license_item_id.license_id.id,
                                         'artist_id': lic_prod.license_item_id.license_id.artist_id.id,
                                         'type': type,
@@ -125,6 +126,7 @@ class AccountMove(models.Model):
                                 data = {
                                         'licensed_item' : lic_prod.license_item_id.id,
                                         'license_product_id': lic_prod.id,
+                                        'invoice_product_id': lic_prod.product_id.id,
                                         'license_id' : lic_prod.license_item_id.license_id.id,
                                         'artist_id': lic_prod.license_item_id.license_id.artist_id.id,
                                         'type': 'not_licensed',
@@ -168,6 +170,7 @@ class AccountMove(models.Model):
                                     data = {
                                             'licensed_item' : lic_prod.license_item_id.id,
                                             'license_product_id': lic_prod.id,
+                                            'invoice_product_id': lic_prod.product_id.id,
                                             'license_id' : lic_prod.license_item_id.license_id.id,
                                             'artist_id': lic_prod.license_item_id.license_id.artist_id.id,
                                             'type': 'not_licensed',
@@ -202,6 +205,7 @@ class AccountMove(models.Model):
                                 data = {
                                         'licensed_item' : lic_prod.license_item_id.id,
                                         'license_product_id': lic_prod.id,
+                                        'invoice_product_id': lic_prod.product_id.id,
                                         'license_id' : lic_prod.license_item_id.license_id.id,
                                         'artist_id': lic_prod.license_item_id.license_id.artist_id.id,
                                         'type': type,
