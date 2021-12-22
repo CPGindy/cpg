7276846b970d3faa9eac5a70a60d39101aee706f
BishalGhimire <bisheshwor.ghimire@selu.edu>
2021-12-22 10:02:33 -0600

    adding zero royalties for components with no license item(10699)

--- ssi_royalty/models/account_move.py
+++ ssi_royalty/models/account_move.py
@@ -211,16 +211,32 @@ class AccountMove(models.Model):
                         # calculate amount of components on which a royalty applies
                         royaltable_components = []
                         inactive_components = []
+                        non_licensed_components = []
                         for component in components:
                             product = component.product_id
                             if any([license.license_item_id.license_status in ['active', 'revise', 'pending'] for license in product.license_product]):
                                 royaltable_components.append(component)
-                            elif any([license.license_item_id.license_status == 'inactive' for license in product.license_product]):
+                            if any([license.license_item_id.license_status == 'inactive' for license in product.license_product]):
                                 inactive_components.append(component)
+                            if not product.license_product:
+                                non_licensed_components.append(component)
 
-                        royaltable_amount = float(invoice_line.price_subtotal) / len(royaltable_components)
+                        #Create Zero royalties for the kit products who dont have license or whose license item are in inactive stages, updated from decision tree on 12/16/2021
+                        if non_licensed_components:
+                            for n_component in non_licensed_components:
+                                data = {
+                                    'invoice_product_id': n_component.product_id.id,
+                                    'type': 'not_licensed',
+                                    'item_value': 0.0,
+                                    'date': date.today(),
+                                    'source_document': rec['name'],
+                                    'payment_status': 'draft',
+                                    'royalty_rate': 0.0,
+                                    'royalty_value': 0.0,
+                                    'invoice_id': rec.id,
+                                }
+                                self.env['ssi_royalty.ssi_royalty'].create(data)
 
-                        #Create Zero royalties for the kit products whose license item are in inactive stages, updated from decision tree on 12/16/2021
                         if inactive_components:
                             inactive_royaltable_amount = float(invoice_line.price_subtotal) / len(inactive_components)
                             for i_component in inactive_components:
@@ -246,72 +262,75 @@ class AccountMove(models.Model):
                                     }
                                     self.env['ssi_royalty.ssi_royalty'].create(data)
 
-                        for r_component in royaltable_components:
-                            artwork_count = len(r_component.product_id.license_product.filtered(
-                                lambda license: license.license_item_id.license_status in ['active', 'revise', 'pending']))
 
-                            for lic_prod in r_component.product_id.license_product.filtered(
-                                lambda license: license.license_item_id.license_status in ['active', 'revise', 'pending']
-                            ):
-                                if lic_prod.license_item_id.license_type != 'flat':
-                                    type = 'sale_on_item'
-                                    royalty_rate = lic_prod.royalty_rate
-                                    royalty_value = (royaltable_amount/artwork_count) * lic_prod.royalty_rate
+                        if royaltable_components:
+                            royaltable_amount = float(invoice_line.price_subtotal) / len(royaltable_components)
+                            for r_component in royaltable_components:
+                                artwork_count = len(r_component.product_id.license_product.filtered(
+                                    lambda license: license.license_item_id.license_status in ['active', 'revise', 'pending']))
 
-                                elif lic_prod.license_item_id.license_type == 'flat':
-                                    type = 'flat_fee'
-                                    royalty_rate = 0.0
-                                    royalty_value = 0.0
+                                for lic_prod in r_component.product_id.license_product.filtered(
+                                    lambda license: license.license_item_id.license_status in ['active', 'revise', 'pending']
+                                ):
+                                    if lic_prod.license_item_id.license_type != 'flat':
+                                        type = 'sale_on_item'
+                                        royalty_rate = lic_prod.royalty_rate
+                                        royalty_value = (royaltable_amount/artwork_count) * lic_prod.royalty_rate
 
-                                data = {
-                                    'licensed_item' : lic_prod.license_item_id.id,
-                                    'license_product_id': lic_prod.id,
-                                    'invoice_product_id': invoice_line.product_id.id,
-                                    'license_id' : lic_prod.license_item_id.license_id.id,
-                                    'artist_id': lic_prod.license_item_id.license_id.artist_id.id,
-                                    'type': type,
-                                    'item_value': royaltable_amount / artwork_count,
-                                    'licensor_id': lic_prod.license_item_id.license_id.licensor_id.id,
-                                    'date': date.today(),
-                                    'source_document': rec['name'],
-                                    'payment_status': 'draft',
-                                    'royalty_rate': royalty_rate,
-                                    'royalty_value': royalty_value,
-                                    'invoice_id': rec.id,
-                                }
-                                royalty = self.env['ssi_royalty.ssi_royalty'].create(data)
-                                for pool in lic_prod.license_item_id.item_pool_id.filtered(lambda p: not p.first_sale_date):
-                                    pool.update({'first_sale_date': date.today()})
+                                    elif lic_prod.license_item_id.license_type == 'flat':
+                                        type = 'flat_fee'
+                                        royalty_rate = 0.0
+                                        royalty_value = 0.0
 
-                                    search_pool_rec = self.env['ssi_royalty.pool'].search([('artist_id', '=', royalty.artist_id.id)])
-                                    if search_pool_rec:
-                                        line_vals = {
-                                            'date': date.today(),
-                                            'memo': pool.art_id.art_license_number,
-                                            'value_type': 'in',
-                                            'pool_value': pool.value,
-                                            'value': pool.value + search_pool_rec.balance,
-                                            'pool_id': search_pool_rec.id
+                                    data = {
+                                        'licensed_item' : lic_prod.license_item_id.id,
+                                        'license_product_id': lic_prod.id,
+                                        'invoice_product_id': invoice_line.product_id.id,
+                                        'license_id' : lic_prod.license_item_id.license_id.id,
+                                        'artist_id': lic_prod.license_item_id.license_id.artist_id.id,
+                                        'type': type,
+                                        'item_value': royaltable_amount / artwork_count,
+                                        'licensor_id': lic_prod.license_item_id.license_id.licensor_id.id,
+                                        'date': date.today(),
+                                        'source_document': rec['name'],
+                                        'payment_status': 'draft',
+                                        'royalty_rate': royalty_rate,
+                                        'royalty_value': royalty_value,
+                                        'invoice_id': rec.id,
+                                    }
+                                    royalty = self.env['ssi_royalty.ssi_royalty'].create(data)
+                                    for pool in lic_prod.license_item_id.item_pool_id.filtered(lambda p: not p.first_sale_date):
+                                        pool.update({'first_sale_date': date.today()})
+
+                                        search_pool_rec = self.env['ssi_royalty.pool'].search([('artist_id', '=', royalty.artist_id.id)])
+                                        if search_pool_rec:
+                                            line_vals = {
+                                                'date': date.today(),
+                                                'memo': pool.art_id.art_license_number,
+                                                'value_type': 'in',
+                                                'pool_value': pool.value,
+                                                'value': pool.value + search_pool_rec.balance,
+                                                'pool_id': search_pool_rec.id
+                                            }
+                                            self.env['ssi_royalty.pool.line'].create(line_vals)
+                                            search_pool_rec.update({'balance': line_vals['value']})
+                                        elif royalty.artist_id:
+                                            pool_val = {
+                                            'artist_id': royalty.artist_id.id,
+                                            'licensor_id': royalty.licensor_id.id or False,
+                                            'balance': 0.0
                                         }
-                                        self.env['ssi_royalty.pool.line'].create(line_vals)
-                                        search_pool_rec.update({'balance': line_vals['value']})
-                                    elif royalty.artist_id:
-                                        pool_val = {
-                                           'artist_id': royalty.artist_id.id,
-                                           'licensor_id': royalty.licensor_id.id or False,
-                                           'balance': 0.0
-                                       }
-                                        pool_id = self.env['ssi_royalty.pool'].create(pool_val)
-                                        line_vals = {
-                                            'pool_id': pool_id.id,
-                                            'date': date.today(),
-                                            'memo': pool.art_id.art_license_number,
-                                            'value_type': 'in',
-                                            'pool_value': pool.value,
-                                            'value': pool.value + pool_id.balance
-                                        }
-                                        self.env['ssi_royalty.pool.line'].create(line_vals)
-                                        pool_id.update({'balance': line_vals['value']})
+                                            pool_id = self.env['ssi_royalty.pool'].create(pool_val)
+                                            line_vals = {
+                                                'pool_id': pool_id.id,
+                                                'date': date.today(),
+                                                'memo': pool.art_id.art_license_number,
+                                                'value_type': 'in',
+                                                'pool_value': pool.value,
+                                                'value': pool.value + pool_id.balance
+                                            }
+                                            self.env['ssi_royalty.pool.line'].create(line_vals)
+                                            pool_id.update({'balance': line_vals['value']})
         return posted
 
 
