01ab83d2c448247db2c978e58be58de1595d1dff
BishalGhimire <mr.ghimire123@gmail.com>
2023-02-16 16:02:05 -0600

    CPG Version upgrade(13298)

--- ssi_product_packaging/models/mrp_bom.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields, api
-
-
-class MrpBom(models.Model):
-    _inherit = 'mrp.bom'
-    
-    
-    product_packaging_id = fields.Many2one('product.packaging',string='Product Packaging')
