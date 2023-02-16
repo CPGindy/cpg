d065287225db55a56f651447183e50b435cf6ef4
BishalGhimire <mr.ghimire123@gmail.com>
2023-02-16 16:02:05 -0600

    CPG Version upgrade(13298)

--- ssi_custom/__manifest__.py
+++ ssi_custom/__manifest__.py
@@ -14,12 +14,16 @@
     'website':  "http://www.ssibtr.com",
     'category': 'SSI',
     'version': '0.1.0',
-    'depends': ['base', 'stock', 'delivery'],
+    'depends': ['base', 'stock', 'delivery', 'account'],
 
     'data': [
+        'security/ir.model.access.csv',
         'report/ssi_stock_picking_report.xml',
         'views/stock_picking_views.xml',
         'views/delivery_carrier_views.xml',
+        'views/product_product_views.xml',
+        'views/sku_lookup_views.xml',
+        'report/ssi_invoice_report.xml'
     ],
     'demo': [
     ],
--- ssi_product_packaging/models/packaging_type.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields, api
-
-
-class PackagingType(models.Model):
-    _name = 'packaging.type'
-    
-    name = fields.Char(string='Name')
