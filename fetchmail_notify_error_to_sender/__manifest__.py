23ba7c78fc4ad867373a6b94f85a0c0b40697301
Chad Thompson <chadt@ssibtr.com>
2023-04-27 15:54:27 -0500

    Add place holder app for fetchmail_notify_error_to_sender

---
+++ fetchmail_notify_error_to_sender/__manifest__.py
@@ -0,0 +1,25 @@
+# Systems Services, Inc.
+
+{
+    'name': "fetchmail_notify_error_to_sender",
+
+    'summary': """
+        Module is a place holder for an old app to allow for v16 conversion""",
+
+    'description': """
+        Customizes base Odoo functionalities and add new one if applies
+    """,
+
+    'author': "",
+    'website':  "http://www.github.com/oca",
+    'version': '0.1.0',
+    'depends': ['base'],
+
+    'data': [
+    ],
+    'demo': [
+    ],
+    'cloc_exclude': [
+        "./**/*"
+    ]
+}
