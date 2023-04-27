c32215cdc7e910bc3ffe343a6419b4f4ac0befda
Chad Thompson <chadt@ssibtr.com>
2023-04-27 15:21:23 -0500

    Add product packaging back for upgrade

---
+++ ssi_product_packaging/__manifest__.py
@@ -0,0 +1,26 @@
+# Systems Services, Inc.
+
+{
+    'name': "SSI Product Packaging",
+
+    'summary': """
+        Module is for customizing base Odoo functionality for CPG""",
+
+    'description': """
+        Customizes base Odoo functionalities and add new one if applies
+    """,
+
+    'author': "Bishal Ghimire - Systems Services Inc",
+    'website':  "http://www.ssibtr.com",
+    'category': 'SSI',
+    'version': '0.1.0',
+    'depends': ['base'],
+
+    'data': [
+    ],
+    'demo': [
+    ],
+    'cloc_exclude': [
+        "./**/*"
+    ]
+}
