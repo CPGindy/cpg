7dc417f0d0251385d8f1cb06c7b46485d6658123
BishalGhimire <mr.ghimire123@gmail.com>
2023-07-21 16:52:02 -0500

    Cleanup(15022)

--- ssi_royalty/models/royalty_royalty.py
+++ ssi_royalty/models/royalty_royalty.py
@@ -89,41 +89,6 @@ class Royalty(models.Model):
         }
 
 
-
-        # for rec in self.filtered(lambda a: not a.is_report_approved):
-        #     if rec.licensor_id:
-        #         royalty_line = [(6, 0, [rec.id])]
-        #         search_report = self.env['ssi_royalty.report'].search([('licensor_id', '=', rec.licensor_id.id), ('status', '=', 'draft')], limit=1)
-        #         if search_report and search_report.status not in ['posted', 'paid']:
-        #             search_report.update({'royalty_line_id' : [(4, rec.id)]})
-        #         else:
-        #             header_vals = {
-        #                 'status': 'draft',
-        #                 'licensor_id': rec.licensor_id.id,
-        #                 'report_date': date.today(),
-        #                 'royalty_line_id' : royalty_line
-        #             }
-        #             search_report = self.env['ssi_royalty.report'].create(header_vals)
-        #         for royalty_line in search_report.royalty_line_id.filtered(lambda l: l.type == "sale_on_item"):
-        #             pool_id = self.env['ssi_royalty.pool'].search([('artist_id', '=', royalty_line.artist_id.id)])
-        #             pool_lines = self.env['ssi_royalty.pool.line'].search([('pool_id', '=', pool_id.id), ('value_type', '=', 'in'), ('first_sale_date', '=', False), ('art_id', '=', royalty_line.licensed_item.id)])
-        #             for pool_line in pool_lines:
-        #                 pool_line.write({'first_sale_date': datetime.now()})
-        #     else:
-        #         raise UserError(_('Please Select Licensor Before Generating Report'))
-        #     rec.payment_status = 'reported'
-
-        # notification = {
-        #     'type': 'ir.actions.client',
-        #     'tag': 'display_notification',
-        #     'params': {
-        #         'title': _('Royalty Report Has Been Successfully Created.'),
-        #         'type': 'success',
-        #         'next': {'type': 'ir.actions.act_window_close'},
-        #     },
-        # }
-        # return notification
-
     def action_reject(self):
         for rec in self.filtered(lambda r: not r.royalty_report_id or r.royalty_report_id.status == 'draft'):
             rec.payment_status = 'rejected'
