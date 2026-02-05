ed2c830901fbd8af9f2978666f4f4098fa3645d7
BishalGhimire <mr.ghimire123@gmail.com>
2026-02-05 13:19:48 -0600

    Initial version upgrade test

--- auditlog/models/log.py
+++ auditlog/models/log.py
@@ -2,6 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo import _, api, fields, models
 from odoo.exceptions import UserError
+from odoo.tools.safe_eval import safe_eval
 
 
 class AuditlogLog(models.Model):
@@ -9,15 +10,16 @@ class AuditlogLog(models.Model):
     _description = "Auditlog - Log"
     _order = "create_date desc"
 
-    name = fields.Char("Resource Name", size=64)
+    name = fields.Char("Resource Name")
     model_id = fields.Many2one(
         "ir.model", string="Model", index=True, ondelete="set null"
     )
     model_name = fields.Char(readonly=True)
     model_model = fields.Char(string="Technical Model Name", readonly=True)
     res_id = fields.Integer("Resource ID")
+    res_ids = fields.Char("Resource IDs")
     user_id = fields.Many2one("res.users", string="User")
-    method = fields.Char(size=64)
+    method = fields.Char()
     line_ids = fields.One2many("auditlog.log.line", "log_id", string="Fields updated")
     http_session_id = fields.Many2one(
         "auditlog.http.session", string="Session", index=True
@@ -48,6 +50,16 @@ class AuditlogLog(models.Model):
             model = self.env["ir.model"].sudo().browse(vals["model_id"])
             vals.update({"model_name": model.name, "model_model": model.model})
         return super().write(vals)
+
+    def show_res_ids(self):
+        self.ensure_one()
+        return {
+            "type": "ir.actions.act_window",
+            "view_mode": "list,form",
+            "res_model": self.model_id.model,
+            "domain": [("id", "in", safe_eval(self.res_ids))],
+            "name": _("Exported Records"),
+        }
 
 
 class AuditlogLogLine(models.Model):
